b5cfd3a15321bd91583c56b3d79ce44a9a84095b
Sp2562 <112017687+Sp2562@users.noreply.github.com>
2026-05-17 18:11:53 +0530

    Update firebase.js

--- career-tracker/src/firebase.js
+++ career-tracker/src/firebase.js
@@ -1,7 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider, signInWithPopup, signOut, onAuthStateChanged } from "firebase/auth";
 import { getFirestore, doc, setDoc, getDoc } from "firebase/firestore";
- 
+
 const firebaseConfig = {
   apiKey: "AIzaSyA207pKFIA-d7S9HdN31-du2CGhbWt6NZQ",
   authDomain: "roadmap-automation-6e4ad.firebaseapp.com",
@@ -10,20 +10,20 @@ const firebaseConfig = {
   messagingSenderId: "1075036356196",
   appId: "1:1075036356196:web:c5b35497b0428f92d96971"
 };
- 
+
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 export const googleProvider = new GoogleAuthProvider();
- 
+
 export const signInWithGoogle = () => signInWithPopup(auth, googleProvider);
 export const logOut = () => signOut(auth);
 export const onAuth = (cb) => onAuthStateChanged(auth, cb);
- 
+
 export async function saveData(uid, data) {
   await setDoc(doc(db, "users", uid), data);
 }
- 
+
 export async function loadData(uid) {
   const snap = await getDoc(doc(db, "users", uid));
   return snap.exists() ? snap.data() : null;
